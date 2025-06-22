81c2011076e138424a18b13699fcb7c86b35e2d1
maxsteel25 <146534377+maxsteel25@users.noreply.github.com>
2025-06-22 19:26:52 +0530

    Update database.sql

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,1 +1,23 @@
+ 
+---data warehouse projects 
 
+use master;
+
+if exists (select 1 from sys.databases where name='datawarehouse')
+begin 
+alter database datawarehouse set single_USER with rollback immediate
+drop database datawarehouse;
+end;
+
+---create database
+create Database datawarehouse;
+
+use datawarehouse;
+
+---we will create schema starting with the first one 
+create schema bronze; 
+go
+create schema silver; 
+go
+create schema gold; 
+go---seprator 
